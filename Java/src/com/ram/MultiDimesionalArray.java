042a0746ed7a35bc034cafc2f88d19b361dc09dd
Ram-Prathyush <ramprathyush.s@gmail.com>
2023-01-09 19:31:17 +0530

    29 jan learning

---
+++ Java/src/com/ram/MultiDimesionalArray.java
@@ -0,0 +1,8 @@
+package com.ram;
+
+public class MultiDimesionalArray {
+public static void main(String[] args) {
+	int[][] myNumbers = { {1,2,3,4,6},{5,7,8,9,10} };
+	System.out.println(myNumbers[1] [4]);
+}
+}

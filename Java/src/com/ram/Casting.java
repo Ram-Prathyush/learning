4130740fbaf4a26b3133f9ff5e364dd2942c4c12
Ram-Prathyush <ramprathyush.s@gmail.com>
2023-01-26 18:23:06 +0530

    mincraft

---
+++ Java/src/com/ram/Casting.java
@@ -0,0 +1,13 @@
+package com.ram;
+
+public class Casting {
+	public static void main(String[] args) {
+		double doubleMy = 78.98d;
+		int intMy = (int) doubleMy;
+		
+		System.out.println(doubleMy);
+		System.out.println(intMy);
+
+	}
+
+}

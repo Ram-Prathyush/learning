2596d44f1fc939412f7e75a3653b658f8ea30172
Ram-Prathyush <ramprathyush.s@gmail.com>
2025-08-25 20:56:20 +0530

    Add password input validation to bonus2.1.py and bonus2.2.py

---
+++ Python/Main/bonus2.1.py
@@ -0,0 +1,6 @@
+password = input('Enter password:')
+
+while password != 'Pass123':
+    password = input('Enter Password:')
+
+print('Password was correct!')
---
+++ Python/Main/bonus2.2.py
@@ -0,0 +1,6 @@
+x = 1
+
+while x <= 6:
+    print(x)
+    x = x+ 1
+

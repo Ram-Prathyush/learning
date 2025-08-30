910b27f02b0bdab3ed76b51dd7ae82b336dc895b
Ram-Prathyush <ramprathyush.s@gmail.com>
2025-08-30 16:50:35 +0530

    Add bonus1.py for title length calculation, bonus2.1.py for password validation, and bonus2.2.py for simple counting loop

--- Python/Main/Todo.py
+++ Python/Main/Todo.py
@@ -3,7 +3,7 @@
 todos = []
 
 while True:
-    user_action = input('Type add show edit or exit todo:')
+    user_action = input('Type add show, edit, exit or complete todo todo:')
     user_action = user_action.strip()
 
     match user_action:
@@ -11,15 +11,18 @@ while True:
             todo = input('Enter a todo:')
             todos.append(todo)
         case'show' | 'display':
-            for item in todos:
+            for index, item in enumerate(todos):
                 item = item.title()
-                print(item)
+                row = f"{index  + 1}- {item}"
+                print(row)
         case ('edit'):
              number = int (input ('Number of the todo to edit: ') )
              number = number - 1
              new_todo = input('Enter new todo: ')
              todos[number] = new_todo
-
+        case 'complete':
+             number = int(input('Number of the todo to complete: '))
+             todos.pop(number)
         case 'exit':
             break
 

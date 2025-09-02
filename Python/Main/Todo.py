8f28682a3c193ef3f6362ce564d0b521bef597d5
Ram-Prathyush <ramprathyush.s@gmail.com>
2025-09-02 21:13:48 +0530

    Refactor Todo.py to initialize todos list and improve file handling for adding and displaying todos. Add initial todos.txt file.

--- Python/Main/Todo.py
+++ Python/Main/Todo.py
@@ -1,16 +1,25 @@
 
-
-todos = []
-
 while True:
     user_action = input('Type add show, edit, exit or complete todo todo:')
     user_action = user_action.strip()
 
     match user_action:
         case'add':
-            todo = input('Enter a todo:')
+            todo = input('Enter a todo:') + "\n"
+
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
+
             todos.append(todo)
+
+            file = open('todos.txt', 'w')
+            file.writelines(todos)
+            file.close()
         case'show' | 'display':
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
             for index, item in enumerate(todos):
                 item = item.title()
                 row = f"{index  + 1}- {item}"

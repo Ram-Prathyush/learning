560e074aa5322467371de6fb65307608af28eb8b
pikacu <120309716+Ram-Prathyush@users.noreply.github.com>
2025-09-07 17:32:46 +0530

    Refactor Todo.py for improved file handling and user input. Update todos list management and fix todo completion logic. Remove initial content from todos.txt. (#64)

--- Python/Main/Todo.py
+++ Python/Main/Todo.py
@@ -1,21 +1,22 @@
 
 while True:
-    user_action = input('Type add show, edit, exit or complete todo todo:')
+    user_action = input('Type add show, edit, exit or complete todo:')
     user_action = user_action.strip()
 
     match user_action:
         case'add':
             todo = input('Enter a todo:') + "\n"
 
-            file = open('todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
+
+
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
 
             todos.append(todo)
 
-            file = open('todos.txt', 'w')
-            file.writelines(todos)
-            file.close()
+
+            with open('todos.txt', 'w')as file:
+                file.writelines(todos)
         case'show' | 'display':
             file = open('todos.txt', 'r')
             todos = file.readlines()
@@ -32,11 +33,19 @@ while True:
         case ('edit'):
              number = int (input ('Number of the todo to edit: ') )
              number = number - 1
+
+             with open('todos.txt', 'r') as file:
+                 todos = file.readlines()
+
              new_todo = input('Enter new todo: ')
-             todos[number] = new_todo
+             todos[number] = new_todo + '\n'
+
+             with open('todos.txt', 'w') as file:
+                 file.writelines(todos)
+
         case 'complete':
              number = int(input('Number of the todo to complete: '))
-             todos.pop(number)
+             todos.pop(number -1)
         case 'exit':
             break
 

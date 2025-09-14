c1b0d0f9958cd66e1cd5cd544596a550db513407
Ram-Prathyush <ramprathyush.s@gmail.com>
2025-09-14 10:53:55 +0530

    Refactor Todo.py to improve command handling and enhance user experience. Add validation for user input and update todos.txt with initial entries. Create bonus9.py for password strength validation.

--- Python/Main/Todo.py
+++ Python/Main/Todo.py
@@ -4,60 +4,75 @@ while True:
     user_action = user_action.strip()
 
 
-    if 'add' in user_action:
+    if user_action.startswith("add"):
         todo = user_action[4:]
-
-
 
         with open('todos.txt', 'r') as file:
             todos = file.readlines()
 
-        todos.append(todo)
-
+        todos.append(todo + '\n')
 
         with open('todos.txt', 'w')as file:
             file.writelines(todos)
-    if 'show' in user_action:
+
+    elif user_action.startswith("show"):
         file = open('todos.txt', 'r')
         todos = file.readlines()
         file.close()
-
-
 
         for index, item in enumerate(todos):
             item = item.strip('\n')
             item = item.title()
             row = f"{index  + 1}- {item}"
             print(row)
-    if 'edit' in user_action:
-         number = int (input ('Number of the todo to edit: ') )
-         number = number - 1
 
-         with open('todos.txt', 'r') as file:
-             todos = file.readlines()
+    elif user_action.startswith("edit"):
+        try:
+            number = int(user_action[5:])
+            print(number)
 
-         new_todo = input('Enter new todo: ')
-         todos[number] = new_todo + '\n'
+            number = number - 1
 
-         with open('todos.txt', 'w') as file:
-             file.writelines(todos)
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
 
-    if 'complete' in user_action:
-         number = int(input('Number of the todo to complete: '))
-         with open('todos.txt', 'r') as file:
-             todos = file.readlines()
-         index = number-1
-         todo_to_remove = todos[index].strip('\n')
-         todos.pop(index)
+            new_todo = input('Enter new todo: ')
+            todos[number] = new_todo + '\n'
 
-         with open('todos.txt', 'w') as file:
-             file.writelines(todos)
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+        except ValueError:
+            print('Command is not valid')
+            continue
 
-         message = f"The todo {todo_to_remove} was removed form the list"
-         print(message)
 
-    if 'exit' in user_action:
+
+    elif user_action.startswith("complete"):
+        try:
+            number = int(user_action[9:])
+
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
+            index = number-1
+            todo_to_remove = todos[index].strip('\n')
+            todos.pop(index)
+
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+
+            message = f"The todo {todo_to_remove} was removed form the list"
+            print(message)
+        except IndexError:
+            print("Index out of range")
+            continue
+
+    elif user_action.startswith("exit"):
         break
+
+    else:
+        print('Command is not valid')
+
+
 
 
 
